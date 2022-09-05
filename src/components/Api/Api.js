da119bb5e1a51b729e981ecd46dd2b9dcaf39e6f
Johnson <johnsonkarasanyi@gmail.com>
2022-09-05 17:09:09 +0200

    add no-cors mode

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -1,4 +1,4 @@
-export const  CreatePerson = (primary_phone, primary_email, full_name) => {
+export const CreatePerson = (primary_phone, primary_email, full_name) => {
   console.log(primary_phone, primary_email, full_name)
   // return async dispatch =>{
   //  const response= await fetch('https://api.proto.cx/case/person', {
@@ -16,14 +16,15 @@ export const  CreatePerson = (primary_phone, primary_email, full_name) => {
   // })
   // if(!response.ok){
   // const resData= await response.json()
-  // console.log(resData)
+  // console.log(resData,"invalid")
   // }
   // const resData= await response.json()
-  // console.log('yes')
+  // console.log(resData,"invalid")
   // dispatch(resData)
   // }
 
-  return fetch('https://api.proto.cx/case/person', {
+  return fetch('http://api.proto.cx/case/person', {
+    mode: 'no-cors',
     method: 'POST',
     headers: {
       'X-SINITIC-TOKEN': 'gAAAAABi92-gD7db3AO6SXAfinsm6cC5_qg0e3mCVyW4z4mQ9z7Gkhtis07wYlwuKbmD5rthvyVtUYU5zFOn-GCozVzNCf9VPH43Cdp5BmoCpMP7_Mj5wNTyuodIAbttzHEEBgfaqpTO',
@@ -32,12 +33,14 @@ export const  CreatePerson = (primary_phone, primary_email, full_name) => {
     body: JSON.stringify({
       casem_id: "casem_WACnAUiz",
       primary_phone: primary_phone,
-      primary_email:primary_email, 
+      primary_email: primary_email,
       full_name: full_name
     })
   })
-    // .then(data => console.log(data.json()))
-    .then(res => console.log(res))
+    .then(res => res.json())
+    .then(data => console.log(data.json()))
+    .catch(error=>console.error(error))
+  // .then(res => console.log(res))
 }
 export const CreateRepresentative = () => {
 
